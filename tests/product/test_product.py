ebbea8e42a32e7105f75039ac3fb52a288a80a21
Wendryo <wendryo.sales@gmail.com>
2022-11-04 13:56:03 -0300

    test: :test_tube: test_create_product implemented

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+import pytest
+
+from inventory_report.inventory.product import Product
+from tests.factories.product_factory import ProductFactory
 
 
-def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+@pytest.fixture
+def stock():
+    return vars(ProductFactory())
+
+
+def test_cria_produto(stock):
+    produto = Product(**stock)
+
+    assert produto.id == stock["id"]
+    assert produto.nome_da_empresa == stock["nome_da_empresa"]
+    assert produto.nome_do_produto == stock["nome_do_produto"]
+    assert produto.data_de_fabricacao == stock["data_de_fabricacao"]
+    assert produto.data_de_validade == stock["data_de_validade"]
+    assert (
+        produto.instrucoes_de_armazenamento
+        == stock["instrucoes_de_armazenamento"]
+    )
