e04caa1af99a2b9377774f0f5e2109bf9b68cc9b
Wendryo <wendryo.sales@gmail.com>
2022-11-04 15:33:33 -0300

    test: :test_tube: test_relatorio_produto Done

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+import pytest
+
+from inventory_report.inventory.product import Product
+from tests.factories.product_factory import ProductFactory
 
 
-def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+@pytest.fixture
+def stock():
+    return vars(ProductFactory())
+
+
+def test_relatorio_produto(stock):
+    produto = Product(**stock)
+
+    assert produto.__repr__() == (
+            f"O produto {produto.nome_do_produto}"
+            f" fabricado em {produto.data_de_fabricacao}"
+            f" por {produto.nome_da_empresa} com validade"
+            f" até {produto.data_de_validade}"
+            f" precisa ser armazenado {produto.instrucoes_de_armazenamento}."
+        )
