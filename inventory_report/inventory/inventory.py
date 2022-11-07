8f59a18ab06911fa60eee01dd4cd031e504ebeb0
Wendryo <wendryo.sales@gmail.com>
2022-11-07 12:05:46 -0300

    feat: :sparkles: Leitores de arquivos no inventário

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,20 +1,40 @@
 import csv
-from inventory_report.reports.simple_report import SimpleReport
+import json
+
+import xmltodict
+
 from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory:
-    def reader(self, file):
-        if file.suffix == ".csv":
-            return self._read_csv(file)
+    @classmethod
+    def reader(cls, file):
+        if ".csv" in file:
+            return cls._read_csv(file)
+        elif ".json" in file:
+            return cls._read_json(file)
+        elif ".xml" in file:
+            return cls._read_xml(file)
 
-    def _read_csv(self, file):
+    @staticmethod
+    def _read_csv(file):
         with open(file, "r") as csv_file:
             return list(csv.DictReader(csv_file))
+
+    @staticmethod
+    def _read_json(file):
+        with open(file, "r") as json_file:
+            return json.load(json_file)
+
+    @staticmethod
+    def _read_xml(file):
+        with open(file, "r") as xml_file:
+            return xmltodict.parse(xml_file.read())["dataset"]["record"]
 
     @classmethod
     def import_data(cls, file: str, type: str):
         if type == "simples":
-            return SimpleReport.generate(cls.reader(file))
+            return SimpleReport.generate(cls.reader(file=file))
         elif type == "completo":
             return CompleteReport.generate(cls.reader(file))
